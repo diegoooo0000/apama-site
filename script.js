5a6947eccb1c9294f152e5c7370164590d234397
diegoooo0000 <diarcarbri@gmail.com>
2026-03-07 15:46:02 -0300

    Refactor category data structure and functions

--- script.js
+++ script.js
@@ -1,79 +1,32 @@
 
-const dados = {
+function mostrarCategoria(cat){
 
-sub9:{
-jogos:[
-"APAMA x APEF Blumenau",
-"APAMA x Spaca Blu",
-"APAMA x Nova Geração Timbó"
-],
-jogadores:[
-{nome:"Arthur Mariani",posicao:"Ala"},
-{nome:"Bernardo Costa",posicao:"Fixo"},
-{nome:"Bruno Franceschi",posicao:"Pivô"}
-]
-},
+const categorias={
 
-sub11:{
-jogos:[
-"APAMA x Cocal do Sul",
-"APAMA x Spaca Blu"
-],
-jogadores:[
-{nome:"Pedro Rossa",posicao:"Ala"},
-{nome:"Gabriel",posicao:"Goleiro"}
-]
-},
+sub7:"Categoria Sub 7 - Iniciação",
 
-sub13:{
-jogos:[
-"APAMA x Bugre do Oeste",
-"APAMA x Marista"
-],
-jogadores:[
-{nome:"Lucas",posicao:"Ala"},
-{nome:"Mateus",posicao:"Fixo"}
-]
-}
+sub8:"Categoria Sub 8",
+
+sub9:"Categoria Sub 9",
+
+sub10:"Categoria Sub 10",
+
+sub11:"Categoria Sub 11",
+
+sub12:"Categoria Sub 12",
+
+sub13:"Categoria Sub 13",
+
+sub14:"Categoria Sub 14",
+
+sub15:"Categoria Sub 15",
+
+sub16:"Categoria Sub 16",
+
+sub17:"Categoria Sub 17"
 
 }
 
-
-function mudarCategoria(cat){
-
-document.getElementById("tituloCategoria").innerText =
-"Jogos - " + cat.toUpperCase()
-
-const jogosDiv = document.getElementById("listaJogos")
-const jogadoresDiv = document.getElementById("listaJogadores")
-
-jogosDiv.innerHTML=""
-jogadoresDiv.innerHTML=""
-
-dados[cat].jogos.forEach(jogo=>{
-
-jogosDiv.innerHTML += `<div class="jogo-card">${jogo}</div>`
-
-})
-
-dados[cat].jogadores.forEach(j=>{
-
-jogadoresDiv.innerHTML +=
-
-`<div class="player">
-<h3>${j.nome}</h3>
-<p>${j.posicao}</p>
-</div>`
-
-})
-
-}
-
-
-function scrollJogos(){
-
-document.getElementById("jogos").scrollIntoView({
-behavior:"smooth"
-})
+document.getElementById("categoria-info").innerHTML="<p>"+categorias[cat]+"</p>"
 
 }
